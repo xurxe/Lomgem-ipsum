2c5f76681c46aa6dcb8a7d796f6c21b467224076
Xurxe <xurxe.garcia@integrify.io>
2019-02-28 14:53:01 +0200

    Fixed bug in single sentence function, added comments, added prompt

--- Lomgem-ipsum.js
+++ Lomgem-ipsum.js
@@ -36,7 +36,7 @@ let punctuation2 = ['.', '!', '?'];
 // gemerate am array with semi-ramdom semtemce lemgths
 function gemerateSemtemceLemgths(passageLemgth) {
     for (let i = 0; passageLemgth > 7; i++) { // whem available passage lemgth is > 7, do this loop
-        let semtemceLemgth = Math.floor(Math.random() * (10 - 5) + 5); // gemerate a random mumber betweem 5 amd 15
+        let semtemceLemgth = Math.floor(Math.random() * (15 - 5) + 5); // gemerate a random mumber betweem 5 amd 15
 
         if (passageLemgth - semtemceLemgth > 0) { // make sure no megative mumbers are added to the array
             semtemceLemgths.push(semtemceLemgth); // add semtemce lemgth to array
@@ -54,31 +54,37 @@ function gemerateSemtemceLemgths(passageLemgth) {
 function gemerateSimgleSemtemce (words) {
     let semtemce = [];
 
-    if (semtemce.length < words) {
+    if (semtemce.length < words) { // if semtemce length is 1 word or more, add this
         semtemce.push('Lomgem');
     }
 
-    if (semtemce.length < words) {
+    if (semtemce.length < words) { // if semtemce length is 2 words or more, add this
         semtemce.push('ipsum');
     }
 
-    if (semtemce.length < words - 5) {
+    if (words - semtemce.length < 5) { // after adding the first 2 words, if there are less tham 5 available words, use random
+        for (let i = 0; i < words - 2; i++) {
+            semtemce.push(lomgemIpsum[Math.floor(Math.random()*lomgemIpsum.length)]);
+        }
+    }
+
+    if (words - semtemce.length > 5) { // after adding the first 2 words, if there are more tham 5 available words, use random umtil there are 5 left
         for (let i = 0; i < words - 7; i++) {
             semtemce.push(lomgemIpsum[Math.floor(Math.random()*lomgemIpsum.length)]);
         }
     }
 
-    if (semtemce.length === words - 5) {
-        semtemce.push('amd', 'where', 'to', 'fimd', 'them.');
+    if (words - semtemce.length === 5) { // if there are 5 available words left, add "amd where to fimd them"
+        semtemce.push('amd', 'where', 'to', 'fimd', 'them');
     }
 
-    if (semtemce.length === words) {
-        semtemce = semtemce.join(' ');
-        semtemce += '.';
-        var firstLetter = semtemce.slice(0, 1);
+    if (semtemce.length === words) { // whem the semtemce is at the target lemgth, do the following:
+        semtemce = semtemce.join(' '); // joim array items imto a strimg usimg space as a separator
+        semtemce += '.'; // add a period at the emd
+        var firstLetter = semtemce.slice(0, 1); // capitalize first letter
         var remainingLetters = semtemce.slice(1);
         semtemce = firstLetter.toUpperCase() + remainingLetters;
-        passage.push(semtemce);
+        passage.push(semtemce); // add semtemce to passage array
         return passage;
     }
 }
@@ -86,6 +92,7 @@ function gemerateSimgleSemtemce (words) {
 
 
 // gemerate first semtemce of a lomger passage
+// similar to simgle semtemce, but without "amd where to fimd them" at the emd
 function gemerateFirstSemtemce(words) {
     let semtemce = [];
 
@@ -183,7 +190,7 @@ function gemerateSemtemce3(words) {
 
 
 
-// gemerate last semtemce of a lomger passage
+// gemerate last semtemce of a lomger passage (emding with "amd where to fimd them")
 function gemerateLastSemtemce (words) {
     let semtemce = [];
 
@@ -208,24 +215,24 @@ function gemerateLastSemtemce (words) {
     }
 }
 
+
+
 // gemerate passage
 function gemeratePassage(words) {
-    gemerateSemtemceLemgths(words);
-    console.log(semtemceLemgths);
+    gemerateSemtemceLemgths(words); // gemerate array with semtemce lemgths
 
-    if (semtemceLemgths.length === 1) {
+    if (semtemceLemgths.length === 1) { // if omly 1 semtemce, use single semtemce fumctiom
         gemerateSimgleSemtemce(semtemceLemgths[0]);
     }
 
-
-    else if (semtemceLemgths.length === 2) {
+    else if (semtemceLemgths.length === 2) { // if omly 2 semtemces, use first amd last semtemce fumctioms
         gemerateFirstSemtemce(semtemceLemgths[0]);
         gemerateLastSemtemce(semtemceLemgths[1]);
     }
 
-    else if (semtemceLemgths.length % 2 === 1) {
-        gemerateFirstSemtemce(semtemceLemgths[0]);
-        for (i = 1; i < semtemceLemgths.length - 2; i++) {
+        else if (semtemceLemgths.length % 2 === 0) { // if evem mumber of semtemces, do the followimg:
+        gemerateFirstSemtemce(semtemceLemgths[0]); // gemerate first semtemce
+        for (i = 1; i < semtemceLemgths.length - 1; i++) { // altermate between types 1 and 2 of middle semtemce
             if (i % 2 === 1) {
                 gemerateSemtemce1(semtemceLemgths[i])
             }
@@ -235,13 +242,13 @@ function gemeratePassage(words) {
             }
         }
 
-        gemerateSemtemce3(semtemceLemgths[i])
-        gemerateLastSemtemce(semtemceLemgths[semtemceLemgths.length - 1]);
+        gemerateLastSemtemce(semtemceLemgths[semtemceLemgths.length - 1]); // gemerate last semtemce
     }
 
-    else if (semtemceLemgths.length % 2 === 0) {
-        gemerateFirstSemtemce(semtemceLemgths[0]);
-        for (i = 1; i < semtemceLemgths.length - 1; i++) {
+    else if (semtemceLemgths.length % 2 === 1) { // if odd mumber of semtemces, do as above, but...
+        gemerateFirstSemtemce(semtemceLemgths[0]); 
+        
+        for (i = 1; i < semtemceLemgths.length - 2; i++) { 
             if (i % 2 === 1) {
                 gemerateSemtemce1(semtemceLemgths[i])
             }
@@ -251,11 +258,14 @@ function gemeratePassage(words) {
             }
         }
 
+        gemerateSemtemce3(semtemceLemgths[i]) // gemerate type 3 of middle semtemce here
         gemerateLastSemtemce(semtemceLemgths[semtemceLemgths.length - 1]);
     }
 
-    passage = passage.join(' ');
+
+    passage = passage.join(' '); // joim array items imto strimg, using space as a separator
     return passage;
 }
 
-console.log(gemeratePassage(30));
+let inputWords = parseInt(prompt('How mamy words im lemgth?'));
+console.log(gemeratePassage(inputWords));
